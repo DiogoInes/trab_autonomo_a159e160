4047a18814a845026e754fb30fcf4d58b960ec8a
diogo <diogo@DESKTOP-I15KL03>
2021-02-18 18:27:18 +0000

    Ficheiro final (correção de comentários)

--- src/m17b_trab_autonomo_a159e160/Main.java
+++ src/m17b_trab_autonomo_a159e160/Main.java
@@ -1,8 +1,14 @@
 
 package m17b_trab_autonomo_a159e160;
-
+/**
+ * 
+ * @author Diogo Inês
+ */
 public class Main {
-
+    /**
+     * Não são usados parâmetros em main().
+     * @param args não se aplica.
+     */
     public static void main(String[] args) {
         Ponto2D p1 = new Ponto2D(5,7);
         Ponto2D p2 = new Ponto2D(2,8);
